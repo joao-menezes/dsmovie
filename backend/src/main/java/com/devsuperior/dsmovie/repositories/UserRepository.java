f1a7d86e63423abb32746bd9fc05d5096c96e9ff
JoaoGabrielMenezes <joaomenezes959@gmail.com>
2022-01-13 15:19:41 -0300

    Save Score

---
+++ backend/src/main/java/com/devsuperior/dsmovie/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsmovie.entities.Score;
+import com.devsuperior.dsmovie.entities.ScorePK;
+import com.devsuperior.dsmovie.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	User findByEmail(String email);
+	
+}
