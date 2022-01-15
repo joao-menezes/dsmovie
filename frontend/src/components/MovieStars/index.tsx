af78a88509dccb604be905a26024e24e0c139067
JoaoGabrielMenezes <joaomenezes959@gmail.com>
2022-01-15 17:21:15 -0300

    Save, useNavigate2

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -30,9 +30,9 @@ function getFills(score: number) {
 }
 
 function Star({fill} : starProps){
-    if (fill == 0) {
+    if (fill === 0) {
         return <StarEmpty />
-    } else if (fill == 1) {
+    } else if (fill === 1) {
         return <StarFull />   
     }else{
         return <StarHalf />
--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,6 +1,5 @@
 import FormCard from 'components/FormCard';
-import { Link, useParams } from 'react-router-dom';
-import { Movie } from 'types/movie';
+import { useParams } from 'react-router-dom';
 
 function Form() {
 
