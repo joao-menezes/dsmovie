0c9ecea56267297176e3dc46709fb5dfcd9f28f4
Joao Gabriel <60220208+joao-menezes@users.noreply.github.com>
2022-04-07 14:17:22 -0300

    Update index.tsx

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -8,7 +8,7 @@ function NavBar(){
         <nav className="container">
             <div className="dsmovie-nav-content">
                 <h1><a href='/'>DSMovie</a></h1>
-                <a href="https://github.com/JoaoGabrielMenezes" target="_blank" rel="noreferrer">
+                <a href="https://github.com/joao-menezes" target="_blank" rel="noreferrer">
                     <div className="dsmovie-contact-container">
                         <GithubIcon />
                         <p className="dsmovie-contact-link">/Joao Menezes</p>
